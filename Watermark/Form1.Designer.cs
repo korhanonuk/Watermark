5d11957e505dd0b01a8097e3f673bfba1ecb1ae7
korhanonuk <konuk16@gmail.com>
2016-08-29 12:11:29 +0300

    Watermark v1.0

---
+++ Watermark/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Watermark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.rb3 = new System.Windows.Forms.RadioButton();
+            this.rb4 = new System.Windows.Forms.RadioButton();
+            this.rb5 = new System.Windows.Forms.RadioButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.pbBack = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(1665, 288);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(63, 17);
+            this.rb1.TabIndex = 1;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "Sağ Üst";
+            this.rb1.UseVisualStyleBackColor = true;
+            this.rb1.CheckedChanged += new System.EventHandler(this.rb1_CheckedChanged);
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(1665, 334);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(59, 17);
+            this.rb2.TabIndex = 2;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "Sol Üst";
+            this.rb2.UseVisualStyleBackColor = true;
+            this.rb2.CheckedChanged += new System.EventHandler(this.rb2_CheckedChanged);
+            // 
+            // rb3
+            // 
+            this.rb3.AutoSize = true;
+            this.rb3.Location = new System.Drawing.Point(1665, 311);
+            this.rb3.Name = "rb3";
+            this.rb3.Size = new System.Drawing.Size(59, 17);
+            this.rb3.TabIndex = 3;
+            this.rb3.TabStop = true;
+            this.rb3.Text = "Sağ Alt";
+            this.rb3.UseVisualStyleBackColor = true;
+            this.rb3.CheckedChanged += new System.EventHandler(this.rb3_CheckedChanged);
+            // 
+            // rb4
+            // 
+            this.rb4.AutoSize = true;
+            this.rb4.Location = new System.Drawing.Point(1665, 357);
+            this.rb4.Name = "rb4";
+            this.rb4.Size = new System.Drawing.Size(55, 17);
+            this.rb4.TabIndex = 4;
+            this.rb4.TabStop = true;
+            this.rb4.Text = "Sol Alt";
+            this.rb4.UseVisualStyleBackColor = true;
+            this.rb4.CheckedChanged += new System.EventHandler(this.rb4_CheckedChanged);
+            // 
+            // rb5
+            // 
+            this.rb5.AutoSize = true;
+            this.rb5.Location = new System.Drawing.Point(1665, 380);
+            this.rb5.Name = "rb5";
+            this.rb5.Size = new System.Drawing.Size(53, 17);
+            this.rb5.TabIndex = 5;
+            this.rb5.TabStop = true;
+            this.rb5.Text = "Ortala";
+            this.rb5.UseVisualStyleBackColor = true;
+            this.rb5.CheckedChanged += new System.EventHandler(this.rb5_CheckedChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl1.LineStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+            this.labelControl1.Location = new System.Drawing.Point(1653, 128);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(210, 38);
+            this.labelControl1.TabIndex = 13;
+            this.labelControl1.Text = "Lütfen eklenecek resmi seçiniz. \r\n(PNG formatında olmalıdır.)";
+            // 
+            // pb5
+            // 
+            this.pb5.Location = new System.Drawing.Point(592, 343);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(450, 231);
+            this.pb5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb5.TabIndex = 11;
+            this.pb5.TabStop = false;
+            // 
+            // pb3
+            // 
+            this.pb3.BackColor = System.Drawing.SystemColors.Control;
+            this.pb3.Location = new System.Drawing.Point(1136, 640);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(450, 231);
+            this.pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb3.TabIndex = 10;
+            this.pb3.TabStop = false;
+            // 
+            // pb4
+            // 
+            this.pb4.Location = new System.Drawing.Point(36, 640);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(450, 231);
+            this.pb4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb4.TabIndex = 9;
+            this.pb4.TabStop = false;
+            // 
+            // pb1
+            // 
+            this.pb1.ErrorImage = null;
+            this.pb1.Location = new System.Drawing.Point(1136, 51);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(450, 231);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb1.TabIndex = 8;
+            this.pb1.TabStop = false;
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(36, 51);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(450, 231);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb2.TabIndex = 7;
+            this.pb2.TabStop = false;
+            // 
+            // pbMain
+            // 
+            this.pbMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbMain.Location = new System.Drawing.Point(12, 12);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(1600, 900);
+            this.pbMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbMain.TabIndex = 0;
+            this.pbMain.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::Watermark.Properties.Resources.image;
+            this.pictureBox1.Location = new System.Drawing.Point(1665, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 85);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::Watermark.Properties.Resources.image;
+            this.pictureBox2.Location = new System.Drawing.Point(1665, 172);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(143, 85);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Image = global::Watermark.Properties.Resources.image;
+            this.pictureBox3.Location = new System.Drawing.Point(1665, 416);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(143, 85);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 17;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl2.LineStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+            this.labelControl2.Location = new System.Drawing.Point(1653, 12);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(155, 19);
+            this.labelControl2.TabIndex = 18;
+            this.labelControl2.Text = "Lütfen ana resmi seçin.";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl3.LineStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+            this.labelControl3.Location = new System.Drawing.Point(1648, 263);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(225, 19);
+            this.labelControl3.TabIndex = 19;
+            this.labelControl3.Text = "Yerleştirmek istediğiniz yeri seçin";
+            // 
+            // pbBack
+            // 
+            this.pbBack.Location = new System.Drawing.Point(12, 12);
+            this.pbBack.Name = "pbBack";
+            this.pbBack.Size = new System.Drawing.Size(710, 400);
+            this.pbBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbBack.TabIndex = 20;
+            this.pbBack.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1042);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.rb5);
+            this.Controls.Add(this.rb4);
+            this.Controls.Add(this.rb3);
+            this.Controls.Add(this.rb2);
+            this.Controls.Add(this.rb1);
+            this.Controls.Add(this.pbMain);
+            this.Controls.Add(this.pbBack);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbMain;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.RadioButton rb3;
+        private System.Windows.Forms.RadioButton rb4;
+        private System.Windows.Forms.RadioButton rb5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb3;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private System.Windows.Forms.PictureBox pbBack;
+
+    }
+}
+
---
+++ Watermark/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Watermark
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //rb1 seçildiğinde pb1 aktif olacak ve diğer pbler inaktif hale geçecek. Ayrıca pb1 in içindeki fotoğrafın opacity ayarı düşecek ve tekrar atanacak.
+        static void PictureBoxActivate(RadioButton rb1, PictureBox pb1, PictureBox pb2, PictureBox pb3, PictureBox pb4, PictureBox pb5, PictureBox pb6)
+        {
+            pb1.Visible = true;
+            pb2.Visible = false;
+            pb3.Visible = false;
+            pb4.Visible = false;
+            pb5.Visible = false;
+
+        }
+
+
+        static public Image SetImageOpacity(Image image, float opacity)
+        {
+
+            try
+            {
+                //create a Bitmap the size of the image provided  
+                Bitmap bmp = new Bitmap(image.Width, image.Height);
+
+                //create a graphics object from the image  
+                using (Graphics gfx = Graphics.FromImage(bmp))
+                {
+
+                    //create a color matrix object  
+                    ColorMatrix matrix = new ColorMatrix();
+
+                    //set the opacity  
+                    matrix.Matrix33 = opacity;
+
+                    //create image attributes  
+                    ImageAttributes attributes = new ImageAttributes();
+
+                    //set the color(opacity) of the image  
+                    attributes.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+
+                    //now draw the image  
+                    gfx.DrawImage(image, new Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, attributes);
+                }
+                return bmp;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+
+        private void rb5_CheckedChanged(object sender, EventArgs e)
+        {
+            PictureBoxActivate(rb5, pb5, pb2, pb3, pb4, pb1, pbMain);
+        }
+
+        private void rb1_CheckedChanged(object sender, EventArgs e)
+        {
+            PictureBoxActivate(rb1, pb1, pb2, pb3, pb4, pb5, pbMain);
+        }
+
+        private void rb3_CheckedChanged(object sender, EventArgs e)
+        {
+            PictureBoxActivate(rb3, pb3, pb2, pb5, pb4, pb1, pbMain);
+        }
+
+        private void rb2_CheckedChanged(object sender, EventArgs e)
+        {
+            PictureBoxActivate(rb2, pb2, pb5, pb3, pb4, pb1, pbMain);
+        }
+
+        private void rb4_CheckedChanged(object sender, EventArgs e)
+        {
+            PictureBoxActivate(rb4, pb4, pb2, pb3, pb5, pb1, pbMain);
+        }
+
+
+        static void SetPictureBox(PictureBox pb1, PictureBox pb2, OpenFileDialog ofd)
+        {
+            pb1.Image = new Bitmap(ofd.FileName);
+            pb2.Controls.Add(pb1);
+            Image img = SetImageOpacity(pb1.Image, 0.6f);
+            pb1.Image = img;
+            pb1.BackColor = Color.Transparent;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Resim Seç";
+                dlg.Filter = "Resim Dosyaları (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+
+                    pbMain.Image = new Bitmap(dlg.FileName);
+                }
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Resim Seçin";
+                dlg.Filter = "PNG Files (*.png)|*.png";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+
+                    SetPictureBox(pb1, pbMain, dlg);
+                    SetPictureBox(pb2, pbMain, dlg);
+                    SetPictureBox(pb3, pbMain, dlg);
+                    SetPictureBox(pb4, pbMain, dlg);
+                    SetPictureBox(pb5, pbMain, dlg);
+                }
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+            Bitmap bitmap = new Bitmap(Convert.ToInt32(1024), Convert.ToInt32(1024), System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            bitmap = (Bitmap)pbMain.Image;
+            Bitmap bitmap2 = new Bitmap(bitmap, new Size(1600, 900));
+            Graphics grfx = Graphics.FromImage(bitmap2);
+            Image img = SetImageOpacity(pb1.Image, 0.6f);
+            pb1.Image = img;
+            pb1.BackColor = Color.Transparent;
+
+            grfx.DrawImage(pb1.Image, 518, 29, 150, 79);
+            // Add drawing commands here
+            
+
+            bitmap2.Save(@"C:\Users\Raven\Desktop\asdsf.jpeg", ImageFormat.Jpeg);
+
+
+
+
+
+            //SaveFileDialog save = new SaveFileDialog();
+
+            ////pb1.Parent = pbMain;
+            ////pb1.ImageLocation = pb1.Location.ToString();
+            ////Bitmap bmp = new Bitmap(pbMain.Image);
+            ////pb1.DrawToBitmap(bmp, pb1.Bounds);
+            ////pbBack.Image = bmp;
+            //save.Filter = "Bitmap files (*.bmp)|*.bmp|JPG files (*.jpg)|*.jpg|GIF files (*.gif)|*.gif|PNG files (*.png)|*.png|TIF files (*.tif)|*.tif|All files (*.*)|*.*";
+            //save.FilterIndex = 2;
+            //save.RestoreDirectory = true;
+            //save.OverwritePrompt = true;
+            //save.ShowHelp = true;
+            //save.AddExtension = true;
+
+            //if ((save.ShowDialog() == DialogResult.OK))
+            //    if (Path.GetExtension(save.FileName).ToLower() == ".bmp")
+            //        pbBack.Image.Save(save.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+            //    else if (Path.GetExtension(save.FileName).ToLower() == ".jpg")
+            //        pbBack.Image.Save(save.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            //    else if (Path.GetExtension(save.FileName).ToLower() == ".gif")
+            //        pbBack.Image.Save(save.FileName, System.Drawing.Imaging.ImageFormat.Gif);
+            //    else if (Path.GetExtension(save.FileName).ToLower() == ".png")
+            //        pbBack.Image.Save(save.FileName, System.Drawing.Imaging.ImageFormat.Png);
+            //    else if (Path.GetExtension(save.FileName).ToLower() == ".tif")
+            //        pbBack.Image.Save(save.FileName, System.Drawing.Imaging.ImageFormat.Tiff);
+
+        }
+
+
+    }
+
+}
